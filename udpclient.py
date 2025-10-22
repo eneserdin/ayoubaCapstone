20052815d33ca02131fe1e832e3c6a794310807b
AbelOberon <ayoubaousseinidk@gmail.com>
2025-10-22 12:11:06 -0500

    Modifications in server shut down handling
    Modified them a little bit

--- udpclient.py
+++ udpclient.py
@@ -3,27 +3,48 @@ import RPi.GPIO as GPIO
 import ADC0834
 from time import sleep
 
-
+# GPIO setup
 GPIO.setmode(GPIO.BCM)
 ADC0834.setup()
 GPIO.setup(20, GPIO.OUT)
 
 serverName = '10.252.251.1'
 serverPort = 12000
-clientSocket = socket(AF_INET, SOCK_DGRAM)
-while True:
-    lightVal=ADC0834.getResult(0)
-    message = bytes(str(lightVal), 'utf-8') # raw_input in python 2
-    clientSocket.sendto(message,(serverName, serverPort))
-    Message, serverAddress = clientSocket.recvfrom(2048)
-    if Message == b'on':
-        GPIO.output(20, True)
-        print("Room too dark! Firing up the Red LED")
-    else:
-        GPIO.output(20, False)
-        print("Enought light! Red LED off!!!")
-        
-    sleep(3)
 
+try:
+    clientSocket = socket(AF_INET, SOCK_DGRAM)
+    clientSocket.settimeout(5.0)
+    
+    print("Client started. Press Ctrl+C to stop.")
+    
+    while True:
+            # Read light sensor
+            lightVal = ADC0834.getResult(0)
+            message = bytes(str(lightVal), 'utf-8')
+            
+            # Send to server
+            clientSocket.sendto(message, (serverName, serverPort))
+            print(f"Sent light value: {lightVal}")
+            
+            # Wait for server response
+            response, serverAddress = clientSocket.recvfrom(2048)
+            
+            # Control LED based on response
+            if response == b'on':
+                GPIO.output(20, True)
+                print(f"Room too dark! (Value: {lightVal}) Firing up the Red LED")
+            elif response == b'off':
+                GPIO.output(20, False)
+                print(f"Enough light! (Value: {lightVal}) Red LED off!!!")
+                
+
+            sleep(3)
+
+except KeyboardInterrupt:
+    print("\nClient shutting down gracefully...")
+
+
+# Cleanup
 clientSocket.close()
 GPIO.cleanup()
+print("GPIO cleaned up and socket closed")
--- udpserver.py
+++ udpserver.py
@@ -1,22 +1,34 @@
 from socket import *
+
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', serverPort))
 print("The server is ready to receive")
 
-on= bytes("on", 'utf-8')
+
+on = bytes("on", 'utf-8')
 off = bytes("off", 'utf-8')
-while 1:
-    message, clientAddress = serverSocket.recvfrom(2048)
-    lightVal = int(message.decode("utf-8"))
-    print (lightVal)
-    if lightVal<= 180:
-       serverSocket.sendto(on, clientAddress)
-    else:
-        serverSocket.sendto(off, clientAddress)
-    
-    
-    
-    
-    #print(modifiedMessage)
-    #serverSocket.sendto(modifiedMessage, clientAddress)
+
+try:
+    while True:
+        message, clientAddress = serverSocket.recvfrom(2048)
+        try:
+            lightVal = int(message.decode("utf-8"))
+            print(f"Received light value: {lightVal} from {clientAddress}")
+            
+            if lightVal <= 180:
+                serverSocket.sendto(on, clientAddress)
+                print(f"Sent: ON (value: {lightVal} <= 180)")
+            else:
+                serverSocket.sendto(off, clientAddress)
+                print(f"Sent: OFF (value: {lightVal} > 180)")
+                
+        except ValueError:
+            print(f"Error: Received non-numeric value: {message}")
+            # Optionally send an error response back to client
+            
+except KeyboardInterrupt:
+    print("\nServer shutting down...")
+finally:
+    serverSocket.close()
+    print("Server socket closed")
